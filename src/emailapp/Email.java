3af7dbf23a04f3c134146bcbd19bdae6017a1122
Ante Dragoje <antekrtalic2@gmail.com>
2021-11-04 20:46:34 +0100

    Creating an email class that holds all nessecary actions

---
+++ src/emailapp/Email.java
@@ -0,0 +1,26 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    String alternativeEmail;
+
+    // Constructor to receive the first and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Ask for the department
+
+    // Generate a random password
+
+    // Set the mailbox capacity
+
+    // Set the alternate email
+
+    // Change the password
+}
---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,7 @@
+package emailapp;
+
+public class EmailApp {
+    public static void main(String[] args) {
+        Email userOne = new Email("Larsen", "McCanley");
+    }
+}
