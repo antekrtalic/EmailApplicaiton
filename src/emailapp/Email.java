18784629598684b61677c1b8dfa8ad3306614713
Ante Dragoje <antekrtalic2@gmail.com>
2021-11-04 20:52:52 +0100

    Modified method for deparment input

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -12,9 +14,20 @@ public class Email {
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        System.out.printf("EMAIL CREATED: " + this.firstName + " " + this.lastName);
     }
 
-    // Ask for the department
+    // Ask for department
+    public String getDepartment() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the deparment:\n1 for Sales\n2 for Development\n3 for Accounting\n4 none of this");
+        int deparmentRole = scanner.nextInt();
+
+        if (deparmentRole == 1) { return "sales"; }
+        else if (deparmentRole == 2) { return "development"; }
+        else if (deparmentRole == 3) { return "accounting"; }
+        else { return ""; }
+    }
 
     // Generate a random password
 
